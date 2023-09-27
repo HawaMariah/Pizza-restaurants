f03a98120cd761c42b748a3cd977086dbb997cb8
Hawa <hawamaria33@gmail.com>
2023-09-27 16:41:23 +0300

    done

--- seed.py
+++ seed.py
@@ -7,14 +7,22 @@ def seed_data():
         db.create_all()
 
         # Add your seed data here
-        restaurant1 = Restaurant(name="Dominion Pizza", address="Good Italian, Ngong Road, 5th Avenue")
-        restaurant2 = Restaurant(name="Pizza Hut", address="Westgate Mall, Mwanzi Road, Nrb 100")
+        restaurant1 = Restaurant(name="KFC", address="Town Moi Avenue")
+        restaurant2 = Restaurant(name="Maveric", address="Syokimau")
+        restaurant3 = Restaurant(name="Pizza Palace", address="123 Main Street")
+        restaurant4 = Restaurant(name="Bella Italia", address="456 Oak Avenue")
 
         pizza1 = Pizza(name="Cheese", ingredients="Dough, Tomato Sauce, Cheese")
         pizza2 = Pizza(name="Pepperoni", ingredients="Dough, Tomato Sauce, Cheese, Pepperoni")
+        pizza3 = Pizza(name="Margherita", ingredients="Dough, Tomato Sauce, Cheese, Basil")
+        pizza4 = Pizza(name="Vegetarian", ingredients="Dough, Tomato Sauce, Cheese, Bell Peppers, Mushrooms, Olives")
+
 
         restaurant_pizza1 = RestaurantPizza(price=5, pizza=pizza1, restaurant=restaurant1)
         restaurant_pizza2 = RestaurantPizza(price=7, pizza=pizza2, restaurant=restaurant1)
+        restaurant_pizza3 = RestaurantPizza(price=8, pizza=pizza3, restaurant=restaurant3)
+        restaurant_pizza4 = RestaurantPizza(price=9, pizza=pizza4, restaurant=restaurant2)
 
-        db.session.add_all([restaurant1, restaurant2, pizza1, pizza2, restaurant_pizza1, restaurant_pizza2])
+
+        db.session.add_all([restaurant1, restaurant2, restaurant3, restaurant4, pizza1, pizza2, pizza3, pizza4, restaurant_pizza1, restaurant_pizza2, restaurant_pizza3, restaurant_pizza4])
         db.session.commit()
