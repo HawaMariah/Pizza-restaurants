531029ccdac5d4cd8aa97b62d98e1373800c4ff4
Hawa <hawamaria33@gmail.com>
2023-09-27 18:16:55 +0300

    final changes

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # Pizza-restaurants
+## Table of Contents
+Installation
+Usage
+Configuration
+Contributing
+License
+
+## Installation
+To run this project on your local machine, follow these steps:
+
+- Clone the Repository:
+git clone https://github.com/yourusername/pizza-restaurant.git
+cd pizza-restaurant
+- Install Dependencies
+- Database Setup:
+This project uses SQLite as the database. No additional setup is required.
+- Run the Application:
+python app.py
+
+The application will be running on http://localhost:5555.
+
+## Usage
+- View Restaurants and Pizzas:
+- Get a Specific Pizza:
+To get information about a specific pizza, use the endpoint: http://localhost:5555/pizzas/{id}, where {id} is the ID of the pizza.
+
+
+## Acknowledgments
+
+This project makes use of the following open-source libraries:
+
+- [Flask](https://flask.palletsprojects.com/): A micro web framework for Python.
+- [SQLAlchemy](https://www.sqlalchemy.org/): A SQL toolkit and Object-Relational Mapping (ORM) system.
+- [Flask-Migrate](https://flask-migrate.readthedocs.io/): A Flask extension for database migrations.
+- [Postman](https://www.postman.com/): A popular API testing tool used during development.
+
--- seed.py
+++ seed.py
@@ -4,13 +4,15 @@ from models import Restaurant, Pizza, RestaurantPizza
 
 def seed_data():
     with app.app_context():
+        # db.drop_all()
         db.create_all()
-
+       
         # Add your seed data here
         restaurant1 = Restaurant(name="KFC", address="Town Moi Avenue")
         restaurant2 = Restaurant(name="Maveric", address="Syokimau")
         restaurant3 = Restaurant(name="Pizza Palace", address="123 Main Street")
         restaurant4 = Restaurant(name="Bella Italia", address="456 Oak Avenue")
+        restaurant5 = Restaurant(name="Peppinos", address="Kenyatta road")
 
         pizza1 = Pizza(name="Cheese", ingredients="Dough, Tomato Sauce, Cheese")
         pizza2 = Pizza(name="Pepperoni", ingredients="Dough, Tomato Sauce, Cheese, Pepperoni")
@@ -22,7 +24,7 @@ def seed_data():
         restaurant_pizza2 = RestaurantPizza(price=7, pizza=pizza2, restaurant=restaurant1)
         restaurant_pizza3 = RestaurantPizza(price=8, pizza=pizza3, restaurant=restaurant3)
         restaurant_pizza4 = RestaurantPizza(price=9, pizza=pizza4, restaurant=restaurant2)
+      
 
-
-        db.session.add_all([restaurant1, restaurant2, restaurant3, restaurant4, pizza1, pizza2, pizza3, pizza4, restaurant_pizza1, restaurant_pizza2, restaurant_pizza3, restaurant_pizza4])
+        db.session.add_all([restaurant1, restaurant2, restaurant3, restaurant4, restaurant5, pizza1, pizza2, pizza3, pizza4, restaurant_pizza1, restaurant_pizza2, restaurant_pizza3, restaurant_pizza4])
         db.session.commit()
